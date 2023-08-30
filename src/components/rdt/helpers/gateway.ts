fc9b395977985c3ea1959ae95d220f174daf58ed
pprog <pprogrammingg@mgmail.com>
2023-08-30 10:18:20 -0400

    draft: gateway helper

--- src/components/rdt/helpers/gateway.ts
+++ src/components/rdt/helpers/gateway.ts
@@ -1,8 +1,31 @@
-import { ResultAsync, err } from 'neverthrow'
 import { rdt } from '../rdt'
 
-export const getEntityDetails = (address: string) =>
-    ResultAsync.fromPromise(
-      rdt.gatewayApi.state.getEntityDetailsVaultAggregated(address),
-      (e: unknown) => e as Error
-    );
+type ResourceData = {
+    resourceAddress: string;
+    metadata: Record<string, any>;
+    amount: number;
+  };
+
+type VaultFungibleItem = {
+    amount: String;
+    last_updated_at_state_version: number,
+    vault_address: String
+}; 
+  
+  const getMetadata = async (accountAddr: string) => {
+    const api = rdt.gatewayApi.state;
+    const resources: ResourceData[] = [];
+    const res = await api.getEntityDetailsVaultAggregated(accountAddr);
+    res.fungible_resources.items.forEach((i: VaultItem) => {
+      if (i.aggregation_level === "Vault") {
+        const amount = i.vaults.items.reduce(
+          (acc: String, curr: String) => acc + Number(curr.amount),
+          0
+        );
+        const metadata = i.explicit_metadata || {};
+        const resourceAddress = i.resource_address;
+        resources.push({ amount, metadata, resourceAddress });
+      }
+    });
+    return resources;
+  };
